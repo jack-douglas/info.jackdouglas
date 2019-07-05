1977ae7903c8e176162718fab8de7dd3fe82790c
Jack Douglas <jack.douglas10@outlook.com>
2019-07-05 22:11:21 +0100

    added shit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="content">
+    <Main/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Main
   }
 }
 </script>
 
 <style>
-#app {
+body {
+  background: #1f2021;
+}
+.content {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #f2f2f2;
+  margin-top: 30vh;
 }
 </style>
